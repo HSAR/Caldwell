84201e83589ab755707f4e69f56966e20530feb3
Shu Sam Chen <research.hyperspace@gmail.com>
2016-11-16 00:46:07 +0000

    Added dev dependencies and base file.

---
+++ src/main/caldwell.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import * as ex from 'excalibur';
+
+var game = new ex.Engine({
+    width: 800,
+    height: 600
+});
+// todo build awesome game here
+
+// Start the engine to begin the game.
+game.start();
