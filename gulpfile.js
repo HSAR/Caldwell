988b6bb4cfd65b67482ca9c4153d8e6147be1b67
Shu Sam Chen <research.hyperspace@gmail.com>
2016-11-16 22:08:42 +0000

    Added sourcemap support.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var sourcemaps = require('gulp-sourcemaps');
 var rimraf = require("rimraf");
 
 var _buildRoot = path.join(__dirname, "build");
@@ -9,8 +10,11 @@ var _testRoot = path.join(__dirname, "build", "src", "test");
 
 gulp.task("build", function () {
     var tsResult = gulp.src("src/**/*.ts")
-        .pipe(ts());
-    return tsResult.js.pipe(gulp.dest("build"));
+        .pipe(sourcemaps.init())
+        .pipe(ts({}));
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("build"));
 });
 
 gulp.task("copyBuildResources", function() {
