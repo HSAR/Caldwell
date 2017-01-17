1f25190d616745353b49a7ba4efe9416bf993465
Shu Sam Chen <sschen@microsoft.com>
2017-01-17 17:42:07 +0000

    Drive initial implementation. TODO: Fuel tanks

---
+++ src/main/model/Drive.ts
@@ -0,0 +1,97 @@
+/// <reference path="../../../typings/index.d.ts" />
+/// <reference path="../../../node_modules/excalibur/dist/excalibur.d.ts" />
+
+import { ActivateableComponent, ActivateableResourceData, SlotConsumer, SlotProvider } from "./Equippable";
+
+import { StaticTextCollection } from "../util/StaticTextCollection";
+
+export class DriveSerialization {
+    constructor(
+        public id:string, // must be globally unique
+        public name:string,
+        public mass:number, // kg
+        public passivePowerDraw:number, // watts
+        public activePowerDraw:number, //watts
+
+        public slotsUsed:string[], 
+        public slotsProvided:string[],
+
+        public maxThrust:number, // N
+        public specificImpulse, // specific fuel consumption * maxThrust
+    ) {
+    }
+}
+
+export class Drive extends ActivateableComponent {
+
+    public static readonly PREFIX:string = "thruster_";
+
+    constructor(
+        id:string, // must be globally unique
+        name:string,
+        mass:number, // kg
+        passivePowerDraw:number, // watts
+        activePowerDraw:number, //watts
+
+        private slotsUsed:string[], 
+        private slotsProvided:string[],
+
+        public maxThrust:number, // N
+        public specificImpulse, // specific fuel consumption * maxThrust
+    ) {
+        super(
+            id, 
+            name,
+            new SlotConsumer(slotsUsed),
+            new SlotProvider(slotsProvided), 
+            new ActivateableResourceData(mass, passivePowerDraw, activePowerDraw)
+        );
+    }
+
+    public getTickRate():number {
+        return 0; // We need to check every tick to update fuel consumption, etc.
+    }
+
+    public activate():void {
+        if (this.internalAmmoConsumer.consumeRounds(1) < 1) { // TODO: Model fuel(!)
+            // TODO: Cut thrust!
+            return;
+        }
+        // TODO: Act on the ship entity in-game
+    }
+
+    private static getDriveMappings():[string, any][] {
+        return Array.from(StaticTextCollection.getObjectsById())
+            .filter((idToObjectMapping:[string, any]) => {
+                return idToObjectMapping[0].startsWith(Drive.PREFIX);
+            });
+    }
+
+    public static getDriveMap():Map<string, Drive> {
+        return new Map<string, Drive>(Drive.getDriveMappings());
+    };
+
+    public static getDrives():Drive[] {
+        return Drive.getDriveMappings().map(StaticTextCollection.mappingToValue);
+    };
+
+    static fromJSON(serialized:DriveSerialization): Drive {
+        return new Drive(
+            serialized.id,
+            serialized.name,
+            serialized.mass,
+            serialized.passivePowerDraw,
+            serialized.activePowerDraw,
+            serialized.slotsUsed,
+            serialized.slotsProvided,
+            serialized.maxThrust,
+            serialized.specificImpulse,
+        );
+    }
+}
+
+console.log(JSON.stringify(
+    [
+        new Drive("drive_small_merlin1D", "Merlin 1D", 630, 0, 0, ["slot_drive_small"], [], 420000, 275),
+    ]
+));
